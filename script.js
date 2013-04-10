694a901e627dc0943dff90766c61587b9b944f91
Anatoli Smol <zarkone@ya.ru>
2013-04-10 12:15:48 +0700

    Wallpaper changer added. List items activation bind added. Some minor changes.

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
+
 //
 // landscape change
 // 
@@ -15,10 +16,13 @@ function changeLandscape(land) {
 var universeMapCtrl = document.getElementById("universeMapCtrl");
 var universeMap = document.getElementById("universeMap");
 var plane = document.getElementById("plane");
+var searchInput = document.getElementById("searchInput");
+var goToCtrl = document.getElementById("goToCtrl");
+var recent = document.getElementById("recent");
 
 universeMapCtrl.onclick = function (){
 	universeMap.style.display = "block";
-	console.log("asd")
+
 
 };
 
@@ -30,9 +34,30 @@ document.onclick = function (event) {
 };
 
 universeMap.onclick = function (event) {
-	console.log(getCosmoCoords(event.clientX, event.clientY));
+	searchInput.value = "(" + getCosmoCoords(event.clientX, event.clientY) + ")";
 	universeMap.style.display = "none";
 
+};
+
+goToCtrl.onclick = function (event) {
+	
+	if(!searchInput.value) return;
+
+	var recentPlaces = recent.getElementsByTagName("li"),
+		recentPlacesLength = recentPlaces.length,
+		i;
+
+	for(i =0; i < recentPlacesLength; i++) {
+
+		if (recentPlaces[i].innerText == searchInput.value) return;
+	}
+
+	var newPlace = document.createElement("li");
+		newPlace.id = "U";
+		newPlace.innerText = searchInput.value;
+		newPlace.onclick = changeWall;
+
+	recent.appendChild(newPlace);
 };
 
 function getCosmoCoords(x,y) {
@@ -70,3 +95,67 @@ normal.onclick = function () {
 
 gravityCtrl.value = 9.8;
 gravityCtrl.onchange();
+
+//
+// Cange Wallpaper
+//
+
+var j, 
+places = [], placeList = [], placeListLength;
+
+
+
+placeList = recent.getElementsByTagName("li");
+placeListLength = placeList.length;
+
+for(j = placeListLength - 1; j >=0; j--) {
+
+	placeList[j].onclick = changeWall;
+	places.push (placeList[j]);
+}
+
+
+function changeWall(event){
+
+	document.body.style.background =  "url('/back/" + event.toElement.id + ".jpg')";
+
+}
+
+// 
+// Activate <li>
+// 
+
+var lists = document.getElementsByTagName("li"),
+	listsLength = lists.length, i;
+
+
+for(i = listsLength - 1; i >= 0; i--) {
+	
+	(function (item) {
+
+		var click = item.onclick || function (){};
+
+		item.onclick = function (event) {
+
+			if(click) { click(event); }
+
+			activate(event);	
+		};
+
+	})(lists[i]);
+}
+
+function activate(event) {
+	var brothers = event.toElement.parentNode.children,
+		brothersLength = brothers.length, i;
+
+	for(i = brothersLength - 1; i >=0; i--) {
+		
+		if (brothers[i] != event.toElement) {
+			brothers[i].classList.remove("active");
+		} else {
+			brothers[i].classList.add("active");
+		}
+	}	
+
+}
